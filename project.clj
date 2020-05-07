6d3b2b7bb9c2ecb9818f8504b825067ed4bd70d5
Roman Smelyansky <romans@madlan.co.il>
2020-05-07 20:55:00 +0300

    version

--- project.clj
+++ project.clj
@@ -1,7 +1,8 @@
-(defproject lein-update-dependency "0.1.3"
+(defproject localize/lein-update-dependency "0.1.2"
   :description "Programmatically update a Leiningen dependency"
   :url "https://github.com/LonoCloud/lein-update-dependency"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[rewrite-clj "0.4.12"]]
+  :plugins [[s3-wagon-private "1.3.1"]]
   :eval-in-leiningen true)
